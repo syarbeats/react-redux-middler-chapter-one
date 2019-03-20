1e2642fec36416c9e092c1e2e80af772f4ec09ba
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-03-20 09:44:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { applyMiddleware, createStore } from 'redux';
 import './App.css';
+
+const reducer = (initialState=0, action) => {
+  if(action.type === 'INC'){
+    return initialState + 1;
+  } else if (action.type === 'DEC'){
+    return initialState - 1;
+  } else if (action.type === 'WTF'){
+    throw new Error("Error Cuy!!");
+  }
+
+  return initialState;
+}
+
+/*All action will be logged by this function*/
+const logger = (store) => (next) => (action) =>{
+  console.log("action fired", action);
+  //action.type = "DEC";
+  next(action);
+  //console.log("store changed", store.getState());
+}
+
+const error = (store) => (next) => (action) => {
+  try{
+    next(action);
+  }catch(e) {
+    console.log("Error!!!", e);
+  }
+}
+
+/*All action will be intercepted by middleware before send to reducer*/
+const middleware =  applyMiddleware(logger, error);
+
+const store = createStore(reducer, 1, middleware);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+});
+
+store.dispatch({type: "INC"});
+store.dispatch({type: "INC"});
+store.dispatch({type: "INC"});
+store.dispatch({type: "DEC"});
+store.dispatch({type: "DEC"});
+store.dispatch({type: "DEC"});
+store.dispatch({type: "WTF"});
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Learning redux middleware</p>
       </div>
     );
   }
